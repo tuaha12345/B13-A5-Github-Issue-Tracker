9aa803fafaf905755569911950c5694ae24a61c9
Syeed md tuaha <syeedmdtuaha@gmail.com>
2026-03-07 17:29:41 +0600

    all Issue done

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+const fetchAllIssue=async()=>{
+    const res=await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data=await res.json();
+    const details=data.data;
+    console.log(data.data);
+    // console.log(data.data.length);
+    totalIssueNumber(data.data.length);
+    displayIssue(details);
+
+}
+
+const displayIssue=(data)=>{
+    const cardContainer=document.getElementById("cardContainer");
+    const allIssue=data;
+    // const card=document.createElement("div");
+    // card.innerHTML="";
+    allIssue.forEach(x => {
+        // console.log(x.title);
+         let statusColor="";
+         let cardStatus=document.getElementById("cardStatus");
+         if(x.status==="open"){
+            statusColor="#1be614";
+         }
+         else if(x.status==="closed"){
+             statusColor="#d00dea";
+         }
+            cardContainer.innerHTML+=`<div class="card bg-white rounded shadow-2xl pt-4 border-t-3 border-t-[${statusColor}] space-y-4 flex flex-col">
+                    <div class="card_top flex items-center justify-between px-3 mt-auto">
+                            <div class="w-6 h-6 bg-transparent" id="cardStatus">
+                                ${x.status==="open"?"<img src='assets/Open-Status.png' class='w-full h-full object-contain'>" :"<img src='assets/Closed- Status .png' class='w-full h-full object-contain'>"}
+                            </div>
+
+                            <div id="proirity">
+                                ${x.priority==="high"?`<button class="btn btn-soft btn-error rounded-2xl py-2 px-6 w-20 h-6">HIGH</button>`:x.priority==="medium"?
+                                    `<button class="btn btn-soft btn-warning rounded-2xl py-2 px-6 w-20 h-6">Medium</button>`:`<button class="btn btn-soft rounded-2xl py-2 px-6 w-20 h-6 text-gray-500">Low</button>`}
+                            </div>
+                    </div>
+                    <div class="card_main px-3 space-y-2 mt-auto">
+                        <h3 class="font-semibold text-[17px]" id="card_title">${x.title}</h3>
+                        <p id="description" class="description-clamp">${x.description}</p>
+                        <div class="flex gap-3">
+                            <button class="btn btn-soft btn-error rounded-2xl py-2 px-6 w-20 h-6"><i class="fa-sharp-duotone fa-solid fa-bug"></i> BUG</button>
+                            <button class="btn btn-soft btn-warning rounded-2xl py-2 px-6  h-6 "> <i class="fa-solid fa-life-ring"></i> HELP WANTED</button>
+                        </div>
+                    </div>
+                    <div class="border-t border-gray-300 mt-auto"></div>
+                    <div class="card_footer px-4 py-3 text-sm text-gray-500 mt-auto flex justify-between">
+                        <div class="">
+                            <p>#${x.id} by ${x.author}</p>
+                            <p>Assignee: ${x.assignee?x.assignee:"Unassigned"}</p>
+                        </div>
+                        <div class="text-right">
+                            <p>${formatDate(x.createdAt)}</p>
+                            <p>Updated: ${formatDate(x.updatedAt)}</p>
+                        </div>
+
+                    </div>
+              </div>`;
+    });
+
+}
+
+const totalIssueNumber=(num)=>{
+    const issueNumber=document.getElementById("issueNumber");
+    issueNumber.innerText=num;
+}
+
+function formatDate(dateString){
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2,'0');
+    const day = String(date.getDate()).padStart(2,'0');
+    return `${year}/${month}/${day}`;
+}
+
+fetchAllIssue();
